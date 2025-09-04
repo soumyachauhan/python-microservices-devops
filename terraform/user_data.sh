1f15f576439b8dcb9f3f3dad3f1441c66d0bc6e3
Soumya Chauhan <soumyachauhan@Soumyas-MacBook-Air.local>
2025-09-04 10:49:12 +0530

    Assignment completed

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -2,6 +2,6 @@
 apt-get update -y
 apt-get install -y docker.io docker-compose
 cd /home/ubuntu
-git clone https://github.com/vineet19102005/python-microservices-devops.git
+git clone https://github.com/soumyachauhan/python-microservices-devops.git
 cd python-microservices-devops
 docker-compose up -d
